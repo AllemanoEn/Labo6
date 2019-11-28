c0ac490eb21cd9a5229e86c1167746b7f20baffe
Enzo <enzo.allemano@hotmail.com>
2019-11-28 14:50:10 +0100

    Version OK
    Régler le prblm des "s" avec cent

--- lab6.cpp
+++ lab6.cpp
@@ -18,6 +18,8 @@ using namespace std;
  */
 string montantEnVaudois(double dblMontant);
 
+string TraductionEntier(string entier);
+
 int main()
 {
     double d;
@@ -29,31 +31,87 @@ int main()
     return 0;
 }
 
+string TraductionEntier(int entier, int iLongueurEntier){
+
+    const string unite[] = {
+        "zero",
+        "un",
+        "deux",
+        "trois",
+        "quatre",
+        "cinq",
+        "six",
+        "sept",
+        "huit",
+        "neuf",
+    };
+
+   const string casSpeciaux[] = {
+        "onze",
+        "douze",
+        "treize",
+        "quatorze",
+        "quinze",
+        "seize",
+        "dix-sept",
+        "dix-huit",
+        "dix-neuf"
+    };
+
+   const string dizaines[]{
+       "cent",
+       "dix",
+       "vingt",
+       "trente",
+       "quarante",
+       "cinqante",
+       "soixante",
+       "septante",
+       "huitante",
+       "nonante",
+   };
+
+   const int CENT = 0;
+
+    string strResultat = "";
+
+    int centaineCalcul = entier/100;
+    int dizaineCalcul = (entier - (centaineCalcul*100))/10;
+    int uniteCalcul = entier - (dizaineCalcul*10 + centaineCalcul*100);
+
+    switch(iLongueurEntier)
+    {
+        case 3:
+            if(centaineCalcul != 1){
+                strResultat += unite[centaineCalcul] + " " + dizaines[CENT] + " ";
+            }else{
+                strResultat += dizaines[CENT] + " ";
+            }
+        case 2:
+            if(uniteCalcul == 0){
+                strResultat += dizaines[dizaineCalcul];
+                break;
+            }else if(uniteCalcul == 1){
+                strResultat += dizaines[dizaineCalcul] + " et ";
+            }else{
+                strResultat += dizaines[dizaineCalcul] + "-";
+            }
+        case 1:
+            if(uniteCalcul == 1){
+                strResultat += unite[uniteCalcul];
+                break;
+            }
+            strResultat += unite[uniteCalcul];
+            break;
+
+        default:
+            break;
+    }
+    return  strResultat;
+}
+
 string montantEnVaudois(double dblMontant){
 
-    string casSpeciaux[] = {
-            "zero",
-            "un",
-            "deux",
-            "trois",
-            "quatre",
-            "cinq",
-            "six",
-            "sept",
-            "huit",
-            "neuf",
-            "dix",
-            "onze",
-            "douze",
-            "treize",
-            "quatorze",
-            "quinze",
-            "seize",
-            "dix-sept",
-            "dix-huit",
-            "dix-neuf",
-            "vingt"
-    };
     int iEntier = dblMontant;
     float fDecimal = dblMontant - iEntier;
     int iDecimal = fDecimal*100;
@@ -61,8 +119,7 @@ string montantEnVaudois(double dblMontant){
     string strEntier = to_string(iEntier);
     string strDecimal = to_string(iDecimal);
 
-    cout << strEntier << "----" << strDecimal;
 
-
+    return TraductionEntier(iEntier,strEntier.length());
 
 }
